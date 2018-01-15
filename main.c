e9420165c0140c3428e7491dff16908e7db956c1
jhold94 <33321625+jhold94@users.noreply.github.com>
2018-01-15 10:51:23 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,82 @@
 
+#include <stdio.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdint.h>
+
+#include "i2c-dev.h"
 
 int bitRead(int value, char bit)
+{
+        return((value << bit) & 0x01);
+}
+
+int fpga_init(char *path, char adr)
+{
+	static int fd =-1;
+	
+	if(fd != -1) return fd;
+	
+	if(path == NULL) {
+		fd = open("/dev/i2c-0", O_RDWR);
+	}
+	
+	if(!adr) adr = 0x28;
+	
+	if(fd != -1) {
+		if (ioctl(fd, I2C_SLAVE_FORCE, 0x28) < 0) {
+			perror("FPGA did not ACK 0x28\n");
+			return -1;
+		}
+	}
+	
+	return fd;
+}
+
+uint8_t fpeek8(int twifd, uint16_t addr)
+{
+	uint8_t data[2];
+	data[0] = ((addr >> 8) & 0xff);
+	data[1] = (addr & 0xff);
+	if (write(twifd, data, 2) != 2) {
+		perror("I2C Addresss set Failed");
+	}
+	read(twifd, data, 1);
+		    
+	return data[0];
+}
+
+int specialDigitalRead(int pin)
+{
+        int devreg = fpeek8(twifd, 0xE);
+        int state;
+        switch(pin)
+        {
+                case 207:
+                        state = bitRead(devreg, 1);
+                        break;
+                case 208:
+                        state = bitRead(devreg, 2);
+                        break;
+                case 209:
+                        state = bitRead(devreg, 3);
+                        break;
+                case 206:
+                        state = bitRead(devreg, 0);
+                        break;
+                default:
+                        break;
+        }
+        return state;
+}
+
+int main(int argc, char ** argv)
+{
+        for(x = 206, x < 210, x++) {
+                printf("Pin %d value = &d\n", x, specialDigitalRead(x));
+        }
+        return 0;
+}
